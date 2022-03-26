9f233415321d73da1d48c59572170472d0677302
Cooper Patton <cpatton@irongalaxystudios.com>
2022-03-26 10:49:24 -0500

    Project setup

---
+++ ProjectMarcus/Source/ProjectMarcus/ProjectMarcus.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectMarcus.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectMarcus, "ProjectMarcus" );
