cb360229ffab2f79a5cb614330a477d1fb50887f
Cooper Patton <cpatton@irongalaxystudios.com>
2022-03-26 13:54:51 -0500

    Adding custom animation instance which will run the anim graph of the player

---
+++ ProjectMarcus/Source/ProjectMarcus/Character/ProjectMarcusAnimInstance.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ProjectMarcusAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARCUS_API UProjectMarcusAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()	
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime); // behaves much like tick function in actors instead of overriding NativeUpdateAnimationProperties
+
+	virtual void NativeInitializeAnimation() override; // kinda like beginPlay for actors but for AnimInstances
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class AProjectMarcusCharacter* PMCharacter;
+
+	void FindOwner();
+
+	// Speed of the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float FootSpeed;
+
+	// Indicates if the character is in the air
+	UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	// Indicates if the character is movement (acceleration > 0)
+	UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsMoving;
+};
---
+++ ProjectMarcus/enc_temp_folder/3cd5c4af6a7664a1516fee3a48f97136/ProjectMarcusAnimInstance.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ProjectMarcusAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARCUS_API UProjectMarcusAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()	
+};
