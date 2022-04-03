7901f03a3f6b35e9044249c1f1e0651455e81320
Cooper Patton <cpatton@irongalaxystudios.com>
2022-04-02 21:14:28 -0500

    Renamed the weapon assets, and started binding some widget properties.

--- ProjectMarcus/Source/ProjectMarcus/Interactables/WeaponItem.h
+++ ProjectMarcus/Source/ProjectMarcus/Interactables/WeaponItem.h
@@ -14,4 +14,10 @@ class PROJECTMARCUS_API AWeaponItem : public AItemBase
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	FName ItemName = FName("Default");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	int32 ItemCount = 0;
 };
---
+++ ProjectMarcus/enc_temp_folder/c8b030dec65979ecc27bdf14e4435fb9/WeaponItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectMarcus/Interactables/ItemBase.h"
+#include "WeaponItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMARCUS_API AWeaponItem : public AItemBase
+{
+	GENERATED_BODY()
+	
+};
